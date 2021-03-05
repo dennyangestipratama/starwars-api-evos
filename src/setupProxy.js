27206f74a7bc9e1ced71d257f9206653ef5f204c
Denny Angesti Pratama <dennyangesti@gmail.com>
2021-03-06 02:07:03 +0700

    try proxy to block cors

---
+++ src/setupProxy.js
@@ -0,0 +1,16 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const BASE_URL = 'http://swapi.dev/api'
+module.exports = function (app) {
+   if (BASE_URL) {
+      app.use(
+         '/api',
+         createProxyMiddleware({
+            target: BASE_URL,
+            changeOrigin: true,
+            pathRewrite: {
+               '^/api': '/',
+            },
+         })
+      )
+   }
+}
